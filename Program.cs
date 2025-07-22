3e1dc6b675008a6cb6f1921147ae03b33f4399b4
ivanna-pm05 <188096251+ivanna-pm05@users.noreply.github.com>
2025-07-21 20:20:10 -0500

    feat: :sparkles: menu
    menu

--- Program.cs
+++ Program.cs
@@ -1,9 +1,9 @@
-﻿using System.Globalization;
+﻿using System;
 internal class Program
 {
     private static void Main(string[] args)
     {
-        Console.WriteLine("Calculadora!");
+        /* Console.WriteLine("Calculadora!");
 
         decimal value = 123.456m;
         Console.OutputEncoding = System.Text.Encoding.UTF8;
@@ -15,7 +15,21 @@ internal class Program
         Console.WriteLine($"{value.ToString("C3", new CultureInfo("ja-JP"))}");
         Console.WriteLine($"-${value.ToString("C3", new CultureInfo("en-US"))}");
         Console.WriteLine($"-{value.ToString("C3", new CultureInfo("fr-FR"))}");
-        Console.WriteLine($"-{value.ToString("C3", new CultureInfo("ja-JP"))}");
+        Console.WriteLine($"-{value.ToString("C3", new CultureInfo("ja-JP"))}"); */
 
+        Console.WriteLine("Welcome to the calculator divisa!");
+        Console.WriteLine("1. Convertir Dolares a Pesos colombianos");
+        Console.WriteLine("2. Convertir Dolares a Euros");
+        Console.WriteLine("3. Convertir Dolares a ienes japoneses");
+        Console.WriteLine("4. Salir");
+
+        Console.WriteLine("Ingrese su nombre:");
+        string nombre = Console.ReadLine();
+
+
+        byte edad = 0;
+        Console.WriteLine("Ingrese su edad:");
+        edad = Convert.ToByte(Console.ReadLine());
+        Console.WriteLine($"Hola {nombre}, tienes {edad} años.");
     }
 }
