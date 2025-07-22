eedb471a1651440c70e5a7c0c9cb72dc312c620d
ivanna-pm05 <188096251+ivanna-pm05@users.noreply.github.com>
2025-07-21 19:45:32 -0500

    feat: :fire: Intentando
    Intentando

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System.Globalization;
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("Calculadora!");
+
+        decimal value = 123.456m;
+        Console.OutputEncoding = System.Text.Encoding.UTF8;
+        Console.WriteLine($"{value.ToString("C3", new CultureInfo("en-US"))}");
+        Console.WriteLine($"{value.ToString("C", new CultureInfo("fr-FR"))}");
+        Console.WriteLine($"{value.ToString("C", new CultureInfo("ja-JP"))}");
+        Console.WriteLine($"${value.ToString("C3", new CultureInfo("en-US"))}");
+        Console.WriteLine($"{value.ToString("C3", new CultureInfo("fr-FR"))}");
+        Console.WriteLine($"{value.ToString("C3", new CultureInfo("ja-JP"))}");
+        Console.WriteLine($"-${value.ToString("C3", new CultureInfo("en-US"))}");
+        Console.WriteLine($"-{value.ToString("C3", new CultureInfo("fr-FR"))}");
+        Console.WriteLine($"-{value.ToString("C3", new CultureInfo("ja-JP"))}");
+
+    }
+}
